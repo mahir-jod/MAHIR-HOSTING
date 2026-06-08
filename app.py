0762c46b6dcb4184088f610570c3be654e6068f6
MAHIR <mahirmosleh54@gmail.com>
2026-06-08 13:00:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -655,4 +655,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
